e211592528796b606fd3ff553822456740ee2615
MusavirAliJ <musavir@integrity.pk>
2021-11-25 12:41:53 +0500

    seller search ui

---
+++ appointment-booking-react/src/Components/Seller/SellerComponent.js
@@ -0,0 +1,9 @@
+import React from "react";
+import NavbarComponent from "../Header/NavbarComponent";
+import SellerSearchComponent from "./SellerSearchComponent";
+
+const SellerComponent = () => {
+    return <SellerSearchComponent />;
+};
+
+export default SellerComponent;
---
+++ appointment-booking-react/src/Components/Seller/SellerSearchComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import NavbarComponent from "../Header/NavbarComponent";
+
+const SellerSearchComponent = () => {
+    return (
+        <Row>
+            <Col>
+                <div>
+                    <SearchFieldComponent />
+                </div>
+            </Col>
+        </Row>
+    );
+};
+
+export default SellerSearchComponent;
